9bc6f68979f750c803ac581071ed2a2ab82d1574
Olya-Kravchuk <143799228+Olya-Kravchuk@users.noreply.github.com>
2025-08-23 14:24:27 +0200

    header link

--- src/drizzle/schema/course.ts
+++ src/drizzle/schema/course.ts
@@ -1,4 +1,4 @@
-import { Relations } from "drizzle-orm"
+import { relations } from "drizzle-orm"
 import { pgTable, text} from "drizzle-orm/pg-core"
 import { createdAt, id, updatedAt } from "../schemaHelpers"
 import { CourseProductTable } from "./courseProduct"
@@ -15,7 +15,7 @@ export const CourseTable = pgTable("courses", {
   updatedAt,
 })
 
-export const CourseRelationships = Relations(CourseTable, ({ many }) => ({
+export const CourseRelationships = relations(CourseTable, ({ many }) => ({
    courseProducts: many(CourseProductTable),
    userCourseAccesses: many(UserCourseAccessTable),
    courseSections: many(CourseSectionTable),
